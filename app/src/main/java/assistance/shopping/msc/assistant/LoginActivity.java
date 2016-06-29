540e8ffc22eabfea6647d7157f6147e589f6758b
omshiv2415 <omshiv2415@yahoo.co.uk>
2016-06-29 21:45:24 +0100

    Google Login Added

--- app/src/main/java/assistance/shopping/msc/assistant/LoginActivity.java
+++ app/src/main/java/assistance/shopping/msc/assistant/LoginActivity.java
@@ -3,6 +3,7 @@ package assistance.shopping.msc.assistant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.View;
@@ -10,22 +11,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends Activity {
+import java.util.Locale;
+
+public class LoginActivity extends Activity implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener{
     private EditText mEmailField;
     private EditText mPasswordField;
     protected Button mButtonLogin;
     protected Button mInitialRegister;
     protected Button mInitialReset;
-    private static final String TAG = "EmailPassword";
-    private FirebaseAuth mAuth;
-    public FirebaseAuth.AuthStateListener mAuthListener;
+    protected Button mGoogleSignIn;
+    private TextToSpeech speech;
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
 
+    private FirebaseAuth mAuth;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +56,33 @@ public class LoginActivity extends Activity {
         mButtonLogin = (Button) findViewById(R.id.buttonLogin);
         mInitialRegister = (Button)findViewById(R.id.buttonInitialRegister);
         mInitialReset = (Button)findViewById(R.id.buttonInitialReset);
+        mGoogleSignIn = (Button)findViewById(R.id.sign_in_button_by_Google);
         mAuth = FirebaseAuth.getInstance();
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                //.enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
+        // [START initialize_auth]
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+
+        mGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takeUserResetPassword = new Intent(LoginActivity.this, GoogleSignInActivity.class);
+               startActivity(takeUserResetPassword);
+
+            }
+        });
         mInitialRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,28 +108,48 @@ public class LoginActivity extends Activity {
                 String email = mEmailField.getText().toString().trim();
                 String password = mPasswordField.getText().toString().trim();
 
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
 
-                                Intent takeUserHome = new Intent(LoginActivity.this, NavigationActivity.class);
-                                startActivity(takeUserHome);
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                if (!task.isSuccessful()) {
-                                    Log.w(TAG, "signInWithEmail", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-                                }
+                if (email.equals("") || password.equals("")) {
+
+                    speech = new TextToSpeech(LoginActivity.this, new TextToSpeech.OnInitListener() {
+                        @Override
+                        public void onInit(int status) {
+                            if (status != TextToSpeech.ERROR) {
+                                speech.setLanguage(Locale.UK);
+                                String toSpeak = ("Please provide correct login Detail");
+                                speech.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+                                Toast.makeText(LoginActivity.this, toSpeak,
+                                        Toast.LENGTH_SHORT).show();
+
 
                             }
-                        });
+                        }
+                    });
+
+                } else {
+
+                    mAuth.signInWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                                    Intent takeUserHome = new Intent(LoginActivity.this, NavigationActivity.class);
+                                    startActivity(takeUserHome);
+                                    // If sign in fails, display a message to the user. If sign in succeeds
+                                    // the auth state listener will be notified and logic to handle the
+                                    // signed in user can be handled in the listener.
+                                    if (!task.isSuccessful()) {
+                                        Log.w(TAG, "signInWithEmail", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                }
+                            });
 
 
-
+                }
             }
         });
 
@@ -96,7 +161,7 @@ public class LoginActivity extends Activity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                    Toast.makeText(LoginActivity.this, "Welcome" + user.getEmail(), Toast.LENGTH_LONG).show();
+                    Toast.makeText(LoginActivity.this, "Welcome " + user.getEmail(), Toast.LENGTH_LONG).show();
                     Intent takeUserResetPassword = new Intent(LoginActivity.this, NavigationActivity.class);
                     startActivity(takeUserResetPassword);
 
@@ -113,7 +178,7 @@ public class LoginActivity extends Activity {
     @Override
     public void onStart() {
         super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
+
     }
 
     @Override
@@ -124,8 +189,80 @@ public class LoginActivity extends Activity {
         }
 
 
+    }
+    // [START onactivityresult]
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if (result.isSuccess()) {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+            } else {
+                // Google Sign In failed, update UI appropriately
+                // [START_EXCLUDE]
 
-}
+                // [END_EXCLUDE]
+            }
+        }
+    }
+    // [END onactivityresult]
 
+    // [START auth_with_google]
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+        // [START_EXCLUDE silent]
+       // showProgressDialog();
+        // [END_EXCLUDE]
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithCredential:onComplete:" + task.isSuccessful());
+
+                        Intent takeUserResetPassword = new Intent(LoginActivity.this, LoginActivity.class);
+                        startActivity(takeUserResetPassword);
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithCredential", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // [START_EXCLUDE]
+                        // hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }
+    // [END auth_with_google]
+
+    // [START signin]
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
